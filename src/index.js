bc3bdf449b31cdb73c1f5c0a72249fa8cd63a8a7
unknown <razmimoj@gmail.com>
2018-11-06 18:25:56 +0330

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Info =({name})=>{
+    return(
+        <div style={{direction:'rtl',textAlign:'center'}}>
+            <h1>اطلاعات</h1>
+            <p> نام کاربری شما : {name}</p>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const authenticatingHOC=(Comp)=>{
+    return (props)=>(
+        <div style={{direction:'rtl',textAlign:'center'}}>
+            {props.isLoggedIn ?(
+                <Comp {...props}/>
+            ):(
+                <p>برای وارد شدن باید لاگین کنید</p>
+            )}
+        </div>
+        
+    )
+}
+
+const AuthInfo = authenticatingHOC(Info)
+
+render(
+    <AuthInfo isLoggedIn={false} name='mojtaba'/>,
+    document.getElementById('root')
+)
